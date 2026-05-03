00852828de2c8ff820e8c5da2a5fb701fc4b3238
mateuus <mateusfernandescarvalhocomim@gmail.com>
2026-05-04 00:38:33 +0200

    https://github.com/mateuus-hub/meu-projeto.git

---
+++ http/plantação.py
@@ -0,0 +1,4 @@
+import random
+
+for i in("6,7,8,9,10"):
+    print(f"o numero de plantas a ser plantada é {random.randint(6,10)}")
---
+++ http/training.py
@@ -0,0 +1,24 @@
+num1 = int(input("digite o prmiero numero"))
+num2 = int(input("digite o segundo numero"))
+
+operacao = input("digite a operaçao que deseja realizar: +,-,*,/")
+
+if operacao == "+":
+    resultado = num1 + num2
+    print("o resultado da soma é:", resultado)
+
+if operacao == "-":
+    resultado = num1 - num2
+    print("o resultado da subtração é:", resultado)
+
+if operacao == "*":
+    resultado = num1 * num2
+    print("o resultado da multiplicação é:", resultado)
+
+if operacao == "/":
+    resultado = num1 / num2
+    print("o resultado da divisão é:", resultado)
+
+print("caralho mano, quão burro você é")    
+         
+        
